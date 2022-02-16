c2dabd9f2c9bea6187dc1a536cb817fb1bc02f93
William Almeida <almeida.ws@hotmail.com>
2022-02-16 20:28:57 -0300

    project setup

---
+++ app/src/main/java/br/com/alura/ceep/NotesList.kt
@@ -0,0 +1,11 @@
+package br.com.alura.ceep
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NotesList : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_notes_list)
+    }
+}
