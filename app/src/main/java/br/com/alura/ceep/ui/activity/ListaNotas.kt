496dd45c949b68fc4712440d3bd5a11cd5f8ada1
William Almeida <almeida.ws@hotmail.com>
2022-02-16 21:05:39 -0300

    class 1.6 - Criando a Activity inicial (include project files from course)

--- app/src/main/java/br/com/alura/ceep/ui/activity/ListaNotas.kt
+++ app/src/main/java/br/com/alura/ceep/ui/activity/ListaNotas.kt
@@ -1,11 +1,13 @@
-package br.com.alura.ceep
+package br.com.alura.ceep.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.alura.ceep.R
 
-class NotesList : AppCompatActivity() {
+class ListaNotas : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_notes_list)
+        setContentView(R.layout.activity_lista_notas)
     }
 }
